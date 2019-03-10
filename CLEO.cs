06a4e37a3ee9f71ddeb22198eaf97257985c112b
Jeroen P. Broks <facebook@tbbs.nl>
2019-03-10 14:12:19 +0100

    Setup basis layout

--- CLEO.cs
+++ CLEO.cs
@@ -24,6 +24,11 @@
 // Version: 19.03.09
 // EndLic
 
+#undef KeyOnExit
+#define ColorStrict  // if set the program will crash when an unknown color has been set!
+#undef Log
+#undef FlagChat
+
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -40,12 +45,15 @@ namespace CLEO {
         readonly byte[] EOLN; // How to separate EOLNS (if system cannot detect this the Unix way will be default).
         readonly FlagParse flags;
         int TabSize => flags.GetInt("tabsize");
+        int docn = 0;
+        int curx = 0;
+        int cury = 0;
 
 
         CLEO(string filename,FlagParse aflags) {
             FileName = filename;
             flags = aflags;
-            if (File.Exists(FileName)) {
+            if (!File.Exists(FileName)) {
                 Console.Write($"File {FileName} does not exist. Create it ? <Y/N> ");
                 var x = Console.ReadKey(true);
                 if (x.Key==ConsoleKey.Y) {
@@ -57,6 +65,74 @@ namespace CLEO {
                     Environment.Exit(1);
                 }
             }
+        }
+
+        void Locate(int x,int y) { // Named after the "LOCATE" command in GWBASIC,although the original command had syntax LOCATE Y,X but as that would only confuse me... :P
+            var rx = x;
+            var ry = y;
+            if (x < 0) rx = Console.WindowWidth + x;
+            if (y < 0) ry = Console.WindowHeight + y;
+            LOG($"Locate({x},{y}) => ({rx},{ry})   -- Winsize: {Console.WindowWidth}x{Console.WindowHeight}");
+            Console.SetCursorPosition(rx, ry);
+        }
+
+        static bool caps = false;
+        static bool NumL = false;
+        void UCaps(bool force = false) {
+            if (force || caps != Console.CapsLock || NumL!=Console.NumberLock) {
+                caps = Console.CapsLock;
+                NumL = Console.NumberLock;
+                QColor("Foot");
+                Locate(5, -1);
+                switch (caps){
+                    case true:  Console.Write("|CAPS|"); break;
+                    case false: Console.Write("|    |"); break;
+                }
+                switch (NumL) {
+                    case true:  Console.Write("|NUM|"); break;
+                    case false: Console.Write("|   |"); break;
+                }
+            }
+        }
+
+        // In C or C++ I would set this INSIDE the void function
+        // but C# doesn't support that (I tried), so too bad!
+        static int ox = 0;
+        static int oy = 0;
+        void UpdateCursorPos(bool force) {
+            if (force || curx!=ox || cury!=oy) {
+                var spos = $"Doc {docn+1}/{CLEOs.Length} Line {cury + 1} Pos {curx + 1} ";
+                QColor("Foot");
+                Locate(-(spos.Length + 2), -1);
+                Console.WriteLine(spos);
+                ox = curx;
+                oy = cury;
+            }
+        }
+
+        void Redraw() {
+            // Clear screen from all junk still living there
+            Cls();
+
+            // Top and bottom bar!
+            QColor("Head"); for (int i = 0; i < Console.WindowWidth - 1; i++) Console.Write(" ");  Locate(0, -1);
+            QColor("Foot"); for (int i = 0; i < Console.WindowWidth - 1; i++) Console.Write(" ");
+
+            // Bar Content
+            QColor("Head");
+            Locate(0, 0);
+            Console.Write(FileName);
+            Locate(-11, 0);
+            Console.Write("F1 = Help");
+            UCaps(true);
+            UpdateCursorPos(true);
+
+
+            // Text Content
+
+            // Position Cursor            
+            Console.SetCursorPosition(curx, cury+1);
+            QColor("Text");
         }
 
 
@@ -72,17 +148,146 @@ namespace CLEO {
          **                                                     **
          *********************************************************
          *********************************************************/
+
+        static string ConfigFile => Dirry.C("$AppSupport$/CLEO/Config.gini");
+        static TGINI config = new TGINI();
+        static void Cls() => Console.Clear();
+
+        static void Crash(string e) {
+            Cls();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("FATAL ERROR!");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(e);
+            Environment.Exit(2);
+        }
+        static void Crash(Exception e) => Crash(e.Message);
+
+        static void Assert(bool cond,string e) {
+            if (!cond) Crash(e);
+        }
+        
         
         static void ShowVersionInfo() {
             Console.WriteLine(MKL.All(true));
         }
 
+        static ConsoleColor GQColor(string n)
+        {
+            switch (n) {
+                case "Black":
+                    return ConsoleColor.Black;
+                case "DBlue":
+                case "DarkBlue":
+                    return ConsoleColor.DarkBlue;
+                case "DGreen":
+                case "DarkGreen":
+                    return ConsoleColor.DarkGreen;
+                case "DRed":
+                case "DarkRed":
+                    return ConsoleColor.DarkRed;
+                case "DMagenta":
+                case "DarkMagenta":
+                    return ConsoleColor.DarkMagenta;
+                case "DCyan":
+                case "DarkCyan":
+                    return ConsoleColor.DarkCyan;
+                case "DYellow":
+                case "DarkYellow":
+                case "Brown":
+                    return ConsoleColor.DarkYellow;
+                case "Gray":
+                case "LGray":
+                case "LightGray":
+                    return ConsoleColor.Gray;
+                case "DarkGray":
+                case "DGray":
+                    return ConsoleColor.DarkGray;
+                case "LBlue":
+                case "LightBlue":
+                case "Blue":
+                    return ConsoleColor.Blue;
+                case "LGreen":
+                case "Green":
+                case "LightGreen":
+                    return ConsoleColor.Green;
+                case "LRed":
+                case "Red":
+                case "LightRed":
+                    return ConsoleColor.Red;
+                case "LMagenta":
+                case "Magenta":
+                case "LightMagenta":
+                    return ConsoleColor.Magenta;
+                case "LCyan":
+                case "Cyan":
+                case "LightCyan":
+                    return ConsoleColor.Cyan;
+                case "Yellow":
+                    return ConsoleColor.Yellow;
+                case "White":
+                    return ConsoleColor.White;
+                default:
+#if ColorStrict
+                    Crash($"I do not know the color {n}");
+                    // The return won't happen anymore, but the C# compiler is not smart enough to realize that, 
+                    // so it has to be present even when the "Crash" routine does take place!
+#endif
+                    return ConsoleColor.Black;
+            }
+        }
+
+        static void QColor(string n) {
+            Assert(config.C($"{n}Color") != "", $"No foreground known for {n}");
+            Assert(config.C($"{n}Back")  != "", $"No background known for {n}");
+            Console.ForegroundColor = GQColor(config.C($"{n}Color"));
+            Console.BackgroundColor = GQColor(config.C($"{n}Back"));
+        }
+
+        static void LoadConfig()
+        {
+            config.D("TextColor", "DGreen");
+            config.D("TextBack", "Black");
+            config.D("HeadColor", "Black");
+            config.D("HeadBack", "DGreen");
+            config.D("FootColor", "Black");
+            config.D("FootBack", "DGreen");
+            if (File.Exists(ConfigFile)) {
+                var confl = QuickStream.LoadLines(ConfigFile);
+                if (confl==null) {
+                    Crash("Invalid config file");
+                }
+                config.ParseLines(confl);
+            }
+        }
+
+#if Log
+        static QuickStream btlog = QuickStream.WriteFile(Dirry.C("$AppSupport$/CLEO/CLEO_DEBUG_LOG.TXT"));
+#endif
+        static void LOG(string l) {
+#if Log
+            btlog.WriteString($"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToShortTimeString()}\t {l}\n", true);
+#endif
+        }
+
         static void Main(string[] args) {
+            LOG("Session Started! Log Created");
+#if Log
+            Console.WriteLine($"PLEASE NOTE! All progress will be logged this time in {Dirry.C("$AppSupport$/ CLEO / CLEO_DEBUG_LOG.TXT")}");
+            Console.Beep();
+#endif
             var fp = new FlagParse(args);
+            LoadConfig();
             fp.CrBool("version", false);
             fp.CrBool("wineoln", false); // When set new files will always have the Windows EOLN otherwise EOLN will be set to the unix way.
             fp.CrInt("tabsize", 8);
+#if FlagChat
+            Console.WriteLine("Parsing cli input");
+            var fpgood = fp.Parse(true);
+            Console.WriteLine();
+#else
             var fpgood = fp.Parse();
+#endif
             MKL.Version("CLEO - CLEO.cs","19.03.09");
             MKL.Lic    ("CLEO - CLEO.cs","GNU General Public License 3");
             Console.WriteLine($"CLEO v{MKL.Newest}");
@@ -100,6 +305,12 @@ namespace CLEO {
             }
             CLEOs = new CLEO[fp.Args.Length];
             for (int i = 0; i < fp.Args.Length; i++) CLEOs[i] = new CLEO(fp.Args[i],fp);
+            CLEOs[0].Redraw(); // Only logic to start with the first document, no?
+#if KeyOnExit
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ReadKey();
+#endif
         }
     }
 
